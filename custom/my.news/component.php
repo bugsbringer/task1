4dd83989a7111108a1c31bbad14bd2025883cb83
bugsbringer <dastins193@gmail.com>
2020-07-28 13:15:13 +0300

    logical fixes

--- custom/my.news/component.php
+++ custom/my.news/component.php
@@ -1,3 +1,76 @@
-<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+CModule::IncludeModule("iblock");
+
+# Обработка запросов
+if ($_POST && array_key_exists("action", $_POST)) {
+	$arResult["isEditAction"] = $_POST["action"] == 'edit';
+	$element = new CIBlockElement;
+
+	switch ($_POST["action"]) {
+		case 'add':
+			$element->Add(
+				Array(
+					"IBLOCK_ID" 	=> $arParams["IBLOCK_ID"],
+					"NAME"			=> $_POST["NAME"],
+					"ACTIVE"		=> "Y",
+					"PREVIEW_TEXT"	=> $_POST["PREVIEW_TEXT"],
+					"DETAIL_TEXT"	=> $_POST["DETAIL_TEXT"],
+					"ACTIVE_FROM"	=> ConvertDateTime(date("d.m.Y H:i:s"), "d.m.Y H:i:s")
+				)
+			);
+
+			break;
+	
+		case 'delete':
+			if (CIBlockElement::GetByID($_POST["ID"])->GetNext()) CIBlockElement::Delete($_POST["ID"]);
+
+			break;
+
+		case 'edit':
+			$arResult["EDITED_ITEM"] = CIBlockElement::GetByID($_POST["ID"])->GetNext();
+
+			break;
+
+		case 'commit_edits':
+			$element->Update(
+				$_POST["ID"], 
+				Array(
+					"NAME"			=> $_POST["NAME"],
+					"PREVIEW_TEXT"	=> $_POST["PREVIEW_TEXT"],
+					"DETAIL_TEXT"	=> $_POST["DETAIL_TEXT"]
+				)
+			);
+
+			break;
+	}
+}
+
+# Логика получения нововстей из БД
+$arIBlockResult = CIBlockElement::GetList(
+	Array("ACTIVE_FROM" => "DESC"), 
+	Array("IBLOCK_ID" => $arParams["IBLOCK_ID"], "ACTIVE" => "Y"), 
+	false, 
+	false, 
+	Array("ID", "NAME", "ACTIVE_FROM", "PREVIEW_TEXT")
+);
+
+# Навигация
+$arIBlockResult->NavStart($arParams['NEWS_COUNT']);
+$arResult["SelectedRowsCount"] = $arIBlockResult->SelectedRowsCount();
+$arResult["NavPrint"] = $arIBlockResult->NavPrint('Новости');
+
+$arResult["ITEMS"] = Array();
+while($arItem = $arIBlockResult->GetNext()) {
+
+    # Получаю кнопки режима правки(добавления, редактирования и удаления)
+    $arButtons = CIBlock::GetPanelButtons($arParams["IBLOCK_ID"], $arItem["ID"]);
+    $arItem["ADD_BUTTON"] = $arButtons["edit"]["add_element"];
+    $arItem["EDIT_LINK"] = $arButtons["edit"]["edit_element"];
+    $arItem["DELETE_LINK"] = $arButtons["edit"]["delete_element"];
+
+    $arResult["ITEMS"][] = $arItem;
+}
+
 $this->IncludeComponentTemplate();
 ?>
--- custom/my.news/templates/.default/template.php
+++ custom/my.news/templates/.default/template.php
@@ -1,92 +1,27 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
 
-<? CModule::IncludeModule("iblock") ?>
-<?$isEditAction = false;?>
 
-<!-- Обработка запросов -->
-<?if ($_POST && array_key_exists("action", $_POST)) {
-	switch ($_POST["action"]) {
-		case 'add':
-			$element = new CIBlockElement;
-
-			$new_artical_array = Array(
-				"IBLOCK_ID" 	=> $arParams["IBLOCK_ID"],
-				"NAME"			=> $_POST["NAME"],
-				"ACTIVE"		=> "Y",
-				"PREVIEW_TEXT"	=> $_POST["PREVIEW_TEXT"],
-				"DETAIL_TEXT"	=> $_POST["DETAIL_TEXT"],
-				"ACTIVE_FROM"	=> ConvertDateTime(date("d.m.Y H:i:s"), "d.m.Y H:i:s")
-			);
-		
-			if (!$element->Add($new_artical_array)) echo "Ошибка: ".$element->LAST_ERROR;
-
-			break;
-	
-		case 'delete':
-			if (CIBlockElement::GetByID($_POST["ID"])->GetNext())
-				CIBlockElement::Delete($_POST["ID"]);
-
-			break;
-
-		case 'edit':
-			$isEditAction = true;
-			$edited_article = CIBlockElement::GetByID($_POST["ID"])->GetNext();
-
-			break;
-
-		case 'commit_edits':
-			$element = new CIBlockElement;
-			$arFields = Array(
-				"NAME"			=> $_POST["NAME"],
-				"PREVIEW_TEXT"	=> $_POST["PREVIEW_TEXT"],
-				"DETAIL_TEXT"	=> $_POST["DETAIL_TEXT"]
-			);
-
-			$element->Update($_POST["ID"], $arFields);
-
-			break;
-	}
-}?>
-
-<!-- Логика вывода новостей -->
-<?
-$arOrder = Array("ACTIVE_FROM" => "DESC"); # Сортировка по дате, по убыванию
-$arSelect = Array("ID", "NAME", "ACTIVE_FROM", "PREVIEW_TEXT");
-$arFilter = Array("IBLOCK_ID" => $arParams["IBLOCK_ID"], "ACTIVE" => "Y");
-
-$articles = CIBlockElement::GetList($arOrder, $arFilter, false, false, $arSelect);
-?>
-
-<!-- Предсталвение вывода элементов -->
 <div>
-<?if (!$articles->SelectedRowsCount()):?>
+<?if (!$arResult["SelectedRowsCount"]):?>
 	<h3>Новостей пока нет</h3>
 <?else:?>
 
 	<!-- Навигация -->
-	<?$articles->NavStart($arParams['NEWS_COUNT']);?>
-	<?if ($arParams['NEWS_COUNT'] && $articles->SelectedRowsCount() > $arParams['NEWS_COUNT']):?>
-		<?=$articles->NavPrint('Новости');?><br><br>
+	<?if ($arParams['NEWS_COUNT'] && $arResult["SelectedRowsCount"] > $arParams['NEWS_COUNT']):?>
+		<?=$arResult["NavPrint"];?><br>
 	<?endif;?>
 	
 	<!-- Верстка элементов -->
-	<?while($article = $articles->GetNextElement()) {
-		$arFields = $article->GetFields();
-
-		# Получаю кнопки режима правки: редактирования и удаления
-		$arButtons = CIBlock::GetPanelButtons($arParams["IBLOCK_ID"], $arFields["ID"]);
-		$arFields["ADD_BUTTON"] = $arButtons["edit"]["add_element"];
-		$arFields["EDIT_LINK"] = $arButtons["edit"]["edit_element"]["ACTION_URL"];
-		$arFields["DELETE_LINK"] = $arButtons["edit"]["delete_element"]["ACTION_URL"];
+	<?foreach ($arResult["ITEMS"] as $arItem) {
 		
-		$this->AddEditAction($arFields['ID'], $arFields['ADD_BUTTON']["ACTION_URL"], $arFields['ADD_BUTTON']["TEXT"]);
-		$this->AddEditAction($arFields['ID'], $arFields['EDIT_LINK'], CIBlock::GetArrayByID($arFields["IBLOCK_ID"], "ELEMENT_EDIT"));
-		$this->AddDeleteAction($arFields['ID'], $arFields['DELETE_LINK'], CIBlock::GetArrayByID($arFields["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+		$this->AddEditAction($arItem['ID'], $arItem['ADD_BUTTON']["ACTION_URL"], $arItem['ADD_BUTTON']["TEXT"]);
+		$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK']["ACTION_URL"], $arItem['EDIT_LINK']["TEXT"]);
+		$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK']["ACTION_URL"], $arItem['DELETE_LINK']["TEXT"], array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 		?>
-		<p id="<?=$this->GetEditAreaId($arFields['ID']);?>">
-			<a><b><?echo $arFields["NAME"]?></b></a><br>
-			<span><?echo $arFields["ACTIVE_FROM"]?></span><br>
-			<?echo $arFields["PREVIEW_TEXT"];?>
+		<p id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+			<a><b><?echo $arItem["NAME"]?></b></a><br>
+			<span><?echo $arItem["ACTIVE_FROM"]?></span><br>
+			<?echo $arItem["PREVIEW_TEXT"];?>
 
 			<!-- Кнопки действий -->
 			<div style="display: flex;">
@@ -94,14 +29,14 @@ $articles = CIBlockElement::GetList($arOrder, $arFilter, false, false, $arSelect
 				<!-- Форма перехода к редактированию статьи -->
 				<form method="post" action="#bottom-form">
 					<input name="action" value="edit" hidden>
-					<input name="ID" value="<?echo $arFields["ID"]?>" hidden>
+					<input name="ID" value="<?echo $arItem["ID"]?>" hidden>
 					<input type="submit" value="Редактировать">
 				</form>
 
 				<!-- Форма удаления статьи -->
 				<form method="post">
 					<input name="action" value="delete" hidden>
-					<input name="ID" value="<?echo $arFields["ID"]?>" hidden>
+					<input name="ID" value="<?echo $arItem["ID"]?>" hidden>
 					<input type="submit" value="Удалить">
 				</form>
 
@@ -113,28 +48,29 @@ $articles = CIBlockElement::GetList($arOrder, $arFilter, false, false, $arSelect
 </div>
 
 <!-- Форма добавления/редактирования статьи -->
-<h2><?if ($isEditAction):?>Изменить<?else:?>Добавить<?endif;?> новость</h2>
+<h2><?if ($arResult["isEditAction"]):?>Изменить<?else:?>Добавить<?endif;?> новость</h2>
 <form method="post" id="bottom-form" action="#">
+
+	<input name="action" value=<?if ($arResult["isEditAction"]):?>"commit_edits"<?else:?>"add"<?endif;?> hidden>
+	<?if ($arResult["isEditAction"]):?><input name="ID" value="<?=$arResult["EDITED_ITEM"]['ID']?>" hidden><?endif;?>
+
     <table style="border-collapse: separate; border-spacing: 10px;">
-		<input name="action" value=<?if ($isEditAction):?>"commit_edits"<?else:?>"add"<?endif;?> hidden>
-			<?if ($isEditAction):?><input name="ID" value="<?=$edited_article['ID']?>" hidden><?endif;?>
         <tr>
             <td>Название</td>
             <td><input type="text" name="NAME" size="61"
-			<?if($isEditAction):?>value="<?=$edited_article["NAME"]?>"<?endif;?>
-			required></td>
+			<?if($arResult["isEditAction"]):?>value="<?=$arResult["EDITED_ITEM"]["NAME"]?>"<?endif;?>required></td>
         </tr>
         <tr>
             <td>Анонс</td>
-            <td><textarea name="PREVIEW_TEXT" id="" cols="60" rows="4" required><?if($isEditAction)echo $edited_article["PREVIEW_TEXT"];?></textarea></td>
+            <td><textarea name="PREVIEW_TEXT" id="" cols="60" rows="4" required><?if($arResult["isEditAction"])echo $arResult["EDITED_ITEM"]["PREVIEW_TEXT"];?></textarea></td>
         </tr>
         <tr>
             <td>Текст</td>
-            <td><textarea name="DETAIL_TEXT" id="" cols="60" rows="4" required><?if($isEditAction)echo $edited_article["DETAIL_TEXT"];?></textarea></td>
+            <td><textarea name="DETAIL_TEXT" id="" cols="60" rows="4" required><?if($arResult["isEditAction"])echo $arResult["EDITED_ITEM"]["DETAIL_TEXT"];?></textarea></td>
         </tr>
         <tr>
             <td><button type="reset">Отмена</button></td>
-            <td><input type="submit" value=<?if ($isEditAction):?>"Cохранить"<?else:?>"Добавить"<?endif;?>></td>
+            <td><input type="submit" value=<?if ($arResult["isEditAction"]):?>"Cохранить"<?else:?>"Добавить"<?endif;?>></td>
         </tr>
     </table>
 </form>
